d93845f8e2f9d80e562f5607822e1759cca57aab
Vladyslav Babych <88340725+vladyslav-babych@users.noreply.github.com>
2022-08-24 12:39:00 +0300

    Refactored colors and theme

--- app/src/main/java/com/otaman/weather/ui/theme/Theme.kt
+++ app/src/main/java/com/otaman/weather/ui/theme/Theme.kt
@@ -2,22 +2,13 @@ package com.otaman.weather.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200,
+    primary = BlueLight,
+    primaryVariant = BlueDark,
+    secondary = BlueLight,
     background = BlueLight
 
     /* Other default colors to override
@@ -31,12 +22,8 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun WeatherTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun WeatherTheme(content: @Composable () -> Unit) {
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
