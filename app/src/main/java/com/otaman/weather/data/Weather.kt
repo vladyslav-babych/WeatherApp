156c03525196aa7b1cc5c4eb52fa0654a1d68344
Vladyslav Babych <88340725+vladyslav-babych@users.noreply.github.com>
2022-09-01 16:24:21 +0300

    Added networking layer

--- app/src/main/java/com/otaman/weather/data/Weather.kt
+++ app/src/main/java/com/otaman/weather/data/Weather.kt
@@ -4,12 +4,12 @@ import com.squareup.moshi.Json
 
 data class Current(
     @Json(name = "temp_c") val temp: Double,
-    @Json(name = "wind_kph") val wind_kph: Double,
+    @Json(name = "wind_kph") val wind: Double,
     @Json(name = "humidity") val humidity: Int,
     @Json(name = "condition") val condition: Condition
 )
 
 data class Condition(
-    @Json(name = "icon") val icon: String,
-    @Json(name = "text") val text: String
+    @Json(name = "icon") val iconCondition: String,
+    @Json(name = "text") val textCondition: String
 )
