af78b06d3b73a4e4204c2c89feff7bbf0c069e9b
Vladyslav Babych <88340725+vladyslav-babych@users.noreply.github.com>
2022-09-15 20:52:24 +0300

    Removed redundant imports

--- app/src/main/java/com/otaman/weather/networking/CurrentWeatherNetworkMapper.kt
+++ app/src/main/java/com/otaman/weather/networking/CurrentWeatherNetworkMapper.kt
@@ -1,21 +1,12 @@
 package com.otaman.weather.networking
 
-import android.os.Build
-import androidx.annotation.RequiresApi
-import androidx.compose.ui.text.toLowerCase
 import com.otaman.weather.domain.weather.CurrentWeather
 import com.otaman.weather.domain.weather.ForecastWeather
 import com.otaman.weather.domain.weather.HourlyWeather
 import com.otaman.weather.networking.responses.CurrentWeatherResponse
 import com.otaman.weather.networking.responses.ForecastResponse
 import com.otaman.weather.networking.responses.ForecastResponse.Forecast.ForecastDay.Hour
-import java.text.DateFormat
 import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.OffsetDateTime
-import java.time.format.DateTimeFormatter
-import java.util.*
 
 fun CurrentWeatherResponse.toCurrentWeather(): CurrentWeather {
     return CurrentWeather(
